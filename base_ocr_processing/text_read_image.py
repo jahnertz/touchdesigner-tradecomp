ef51f48f392c7e1a018d13d5fd1ce510fcdd2e77
Jordan Hanrahan <jordan.hanrahan@icloud.com>
2022-07-14 12:57:17 +1000

    pre-merge with windozed version

--- base_ocr_processing/text_read_image.py
+++ base_ocr_processing/text_read_image.py
@@ -3,4 +3,6 @@ import pytesseract as pyt
 
 pyt.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract'
 
-print(pyt.image_to_string(Image.open('ocr_source.png')))
+ocr_result = pyt.image_to_string(Image.open('ocr_source.png'))
+print(ocr_result)
+op('text_ocr_result')[0,0] = ocr_result
