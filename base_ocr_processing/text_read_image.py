f8c4bbb5f40cd588b278f10f86ceda7ca369f5d5
Jordan Hanrahan <jordan.hanrahan@icloud.com>
2022-07-14 15:11:06 +1000

    added some validation & processing to ocr result

--- base_ocr_processing/text_read_image.py
+++ base_ocr_processing/text_read_image.py
@@ -1,10 +1,17 @@
 from PIL import Image
 import pytesseract as pyt
+import re
 
 tesseract_exe = tdu.expandPath(ipar.ExtPython.Tesseractexe)
 
 pyt.pytesseract.tesseract_cmd = r"%s" % tesseract_exe
 
 ocr_result = pyt.image_to_string(Image.open('ocr_source.png'))
-print(ocr_result)
-op('text_ocr_result')[0,0] = ocr_result
+if re.match(ocr_result, "^\s*$"):
+    ocr_result = "ERROR: No readable text found."
+else:
+    ocr_result = re.sub('\s+', '', ocr_result)
+
+equity = "$" + "{:,}".format(float(ocr_result))
+print(equity)
+op('text_ocr_result')[0,0] = equity
